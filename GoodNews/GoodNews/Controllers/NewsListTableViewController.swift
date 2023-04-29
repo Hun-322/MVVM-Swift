1a6c1fec68890514095582e1643c623c8af1fe9c
김세훈 <ksh26271@naver.com>
2023-04-30 05:02:18 +0900

    [추가] GoddNews 프로젝트 생성

---
+++ GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNews
+//
+//  Created by KSH on 2023/04/30.
+//
+
+import UIKit
+
+class NewsListTableViewControllers: UITableViewController {
+    
+}
