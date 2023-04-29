5b04331db52894be54f7882e3c428330a215e845
김세훈 <ksh26271@naver.com>
2023-04-30 05:42:44 +0900

    [추가] 기본 UI 추가

--- GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
+++ GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
@@ -9,4 +9,14 @@ import UIKit
 
 class NewsListTableViewControllers: UITableViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        setup()
+    }
+    
+    private func setup() {
+        // NavigationBar Title 사이즈 크게 변경
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
 }
