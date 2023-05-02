f22eefbf4b92671b5eabdd3732f936e775750a53
김세훈 <ksh26271@naver.com>
2023-05-02 20:33:50 +0900

    [추가] Controllers 추가

--- HotCoffee/HotCoffee/Controllers/AddOrderNewController.swift
+++ HotCoffee/HotCoffee/Controllers/AddOrderNewController.swift
@@ -7,6 +7,93 @@
 
 import UIKit
 
+protocol AddCoffeeOrderDelegate {
+    func addCoffeeOrderViewControllerDidSave(order: Order, controller: UIViewController)
+    func addCoffeeOrderViewControllerDidClose(controller: UIViewController)
+}
+
 class AddOrderViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     
+    var delegate: AddCoffeeOrderDelegate?
+    
+    private var coffeeSizesSegmentedControl: UISegmentedControl!
+    private var vm = AddCoffeeOrderViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        coffeeSizesSegmentedControl = UISegmentedControl(items: self.vm.sizes)
+        coffeeSizesSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(coffeeSizesSegmentedControl)
+        
+        coffeeSizesSegmentedControl.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 20).isActive = true
+        coffeeSizesSegmentedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    @IBAction func close() {
+        if let delegate = self.delegate {
+            delegate.addCoffeeOrderViewControllerDidClose(controller: self)
+        }
+    }
+    
+    @IBAction func save() {
+        let name = nameTextField.text
+        let email = emailTextField.text
+        
+        let selectedSize = coffeeSizesSegmentedControl.titleForSegment(at: coffeeSizesSegmentedControl.selectedSegmentIndex)
+        
+        guard let indexPath = tableView.indexPathForSelectedRow else {
+            fatalError("커피 선택 오류입니다!")
+        }
+        
+        vm.name = name
+        vm.email = email
+        vm.selectedSize = selectedSize
+        vm.selectedType = vm.types[indexPath.row]
+        
+        Webservice().load(resource: Order.create(vm: vm)) { result in
+            switch result {
+            case .success(let order):
+                
+                if let order = order, let delegate = self.delegate {
+                    DispatchQueue.main.async {
+                        delegate.addCoffeeOrderViewControllerDidSave(order: order, controller: self)
+                    }
+                }
+                
+            case.failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+extension AddOrderViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.vm.types.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoffeeTypeTableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = self.vm.types[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+    
+    // 특정 셀이 선택 해제되었을 때 호출되는 메서드
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    }
 }
