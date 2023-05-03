92d78f43c7229930bc151e44054f6a60c820e919
김세훈 <ksh26271@naver.com>
2023-05-03 23:01:37 +0900

    [추가] GoodWeather App 프로젝트 생성

---
+++ GoodWeather/GoodWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by KSH on 2023/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
