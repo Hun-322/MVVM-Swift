5d0073da28e5c5e99784f7ff5b7794ab294a744d
김세훈 <ksh26271@naver.com>
2023-05-04 19:59:22 +0900

    [추가] TableView UI 추가

--- GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
+++ GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -8,5 +8,23 @@
 import UIKit
 
 class AddWeatherCityViewController: UIViewController {
+    @IBOutlet weak var cityNameTextField: UITextField!
     
+    @IBAction func saveCityButtonPressed() {
+        if let city = cityNameTextField.text {
+            let weatherURL = URL(string: "")!
+            
+            let weatherResource = Resoucre<Any>(url: weatherURL) { data in
+                return data
+            }
+            
+            WebService().laod(resource: weatherResource) { reuslt in
+                
+            }
+        }
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true)
+    }
 }
--- GoodWeather/GoodWeather/Controllers/WeatherListTableViewController.swift
+++ GoodWeather/GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -13,6 +13,37 @@ class WeatherListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let resource = Resoucre<WeatherResponse>(url: URL(string: "")!) { data in
+            
+            return try? JSONDecoder().decode(WeatherResponse.self, from: data)
+        }
+        
+        WebService().laod(resource: resource) { weatherResponse in
+            if let weatherResponse = weatherResponse {
+                print(weatherResponse)
+            }
+        }
+    }
+}
 
+extension WeatherListTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+        
+        cell.cityNameLabel.text = "Korea"
+        cell.temperatureLabel.text = "70°"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
